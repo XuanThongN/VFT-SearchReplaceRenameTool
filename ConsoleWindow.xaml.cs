fa40e2075b5f5f040dd02a282184016783a144cd
XuanThongN <thong.xuan.vft@gmail.com>
2023-11-20 00:24:28 +0700

    add project files

---
+++ ConsoleWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace SearchReplaceRename
+{
+    public partial class ConsoleWindow : Window
+    {
+        public ConsoleWindow(string path, string search, string replace)
+        {
+            Path = path;
+            Search = search;
+            Replace = replace;
+
+            InitializeComponent();
+        }
+
+        public string Path { get; set; }
+        public string Search { get; set; }
+        public string Replace { get; set; }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            var worker = new BackgroundWorker();
+            worker.DoWork += DoWork;
+            worker.RunWorkerCompleted += RunWorkerCompleted;
+            worker.RunWorkerAsync();
+        }
+
+        private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs args)
+        {
+            MessageBox.Show("Replacement completed!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs args)
+        {
+            if (!Directory.Exists(Path))
+            {
+                WriteLine("ERROR: Path does not exist. Please select a valid path and try again");
+            }
+            else
+            {
+                WriteLine(string.Format(@"Renaming all files and directories containing '{0}' to have '{1}' instead in path '{2}'", Search, Replace, Path));
+                SearchAndReplaceRename(Search, Replace, Path);
+            }
+        }
+
+        private void WriteLineSync(string line)
+        {
+            textBox1.Text += "\n" + line;
+        }
+
+        private void WriteLine(string line)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action<string>(WriteLineSync), line);
+        }
+
+        private void SearchAndReplaceRename(string search, string replace, string path)
+        {
+            string[] excludedFolders = { "bin", ".vs", ".git", "Logs" };
+            string[] allowedExtensions = { ".cs", ".cshtml", ".csproj", ".csproj.user", ".sln" };
+
+            if (excludedFolders.Any(folder => LastPathSegment(path).Equals(folder, StringComparison.OrdinalIgnoreCase)))
+            {
+                WriteLine($"Skipping folder: {path}");
+                return;
+            }
+
+            foreach (var file in Directory.GetFiles(path).Where(file => allowedExtensions.Any(ext => file.EndsWith(ext, StringComparison.OrdinalIgnoreCase))))
+            {
+                RenameIfMatch(file, search, replace);
+                ReplaceContent(file, search, replace);
+            }
+
+            foreach (var directory in Directory.GetDirectories(path))
+            {
+                SearchAndReplaceRename(search, replace, directory);
+                RenameIfMatch(directory, search, replace);
+            }
+        }
+
+        private static string LastPathSegment(string path)
+        {
+            var parts = path.Split('\\');
+            return parts.Length > 0 ? parts[parts.Length - 1] : path;
+        }
+
+        private static void RenameIfMatch(string path, string search, string replace)
+        {
+            try
+            {
+                var name = System.IO.Path.GetFileName(path);
+                if (name.Contains(search))
+                {
+                    var newName = name.Replace(search, replace);
+                    var newPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(path), newName);
+                    Directory.Move(path, newPath);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+
+        }
+
+        private static void ReplaceContent(string file, string search, string replace)
+        {
+            try
+            {
+                var content = File.ReadAllText(file);
+                if (content.Contains(search))
+                {
+                    content = content.Replace(search, replace);
+                    File.WriteAllText(file, content);
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message); ;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+
+namespace SearchReplaceRename
+{
+  /// <summary>
+  ///   Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnChooseFile_Click(object sender, RoutedEventArgs e)
+    {
+      var dialog = new FolderBrowserDialog();
+
+      if (System.Windows.Forms.DialogResult.OK == dialog.ShowDialog())
+        tbPath.Text = dialog.SelectedPath;
+    }
+
+    private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      CheckIfReadyToGo();
+    }
+
+    private void tbReplace_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      CheckIfReadyToGo();
+    }
+
+    private void tbPath_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      CheckIfReadyToGo();
+    }
+
+    private void CheckIfReadyToGo()
+    {
+      btnGo.IsEnabled = 0 != tbSearch.Text.Length && 0 != tbReplace.Text.Length && 0 != tbPath.Text.Length;
+    }
+
+    private void btnGo_Click(object sender, RoutedEventArgs e)
+    {
+      if (MessageBoxResult.Yes ==
+          MessageBox.Show(
+            "Bạn có chắc chắn muốn tiếp tục",
+            "Cảnh báo!", MessageBoxButton.YesNo))
+      {
+        var dialog = new ConsoleWindow(tbPath.Text, tbSearch.Text, tbReplace.Text);
+        dialog.ShowDialog();
+      }
+    }
+  }
+}
